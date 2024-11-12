6f55bbea94eb01e2e1856003edd7a4d1daa6661a
ali-raza-baig <alirazabaigmdk@gmail.com>
2024-11-12 15:40:18 +0500

    client

--- index.js
+++ index.js
@@ -4,13 +4,24 @@ const app = express()
 import dotenv from "dotenv"
 import DbConnection from "./Config/Database.js"
 import authRoutes from "./Routes/authRoutes.js"
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 dotenv.config()
 // Database Config
 DbConnection()
 
-app.get("/", (req, res) => {
-    res.send("Good Ali")
-})
+// Determine __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files from the Vite build directory
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
+// Route all requests to the React app
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 // Routes 
 app.use("/api/v1/", authRoutes)
 
