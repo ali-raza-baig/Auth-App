be1086ac97e092af5308ade1644b24edeb672978
Ahmad <95245638+Ahmaddar007@users.noreply.github.com>
2024-11-13 17:45:13 +0500

    width

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -4,13 +4,13 @@ function Signup() {
   return (
     <div>
        <div className="flex flex-col justify-center items-center">
-        <form className="bg-slate-400 flex flex-col gap-5 my-20 p-12 rounded">
+        <form className="bg-slate-400 flex flex-col gap-5 my-20 p-12 rounded w-[40rem]">
           <h2 className="text-4xl text-center">SIGNUP</h2>
           <div className="flex flex-col gap-1">
             <label>Username</label>
             <input
               type="text"
-              className="bg-gray-200 p-3 rounded w-96"
+              className="bg-gray-200 p-3 rounded w-[33rem]"
               placeholder="Enter your UserName"
             />
           </div>
@@ -18,7 +18,7 @@ function Signup() {
             <label>Email</label>
             <input
               type="Email"
-              className="bg-gray-200 p-3 rounded w-96"
+              className="bg-gray-200 p-3 rounded w-[33rem]"
               placeholder="Enter your Email"
             />
           </div>
@@ -26,7 +26,7 @@ function Signup() {
             <label>Password</label>
             <input
               type="Email"
-              className="bg-gray-200 p-3 rounded w-96"
+              className="bg-gray-200 p-3 rounded w-[33rem]"
               placeholder="Enter your Password"
             />
           </div>
@@ -34,12 +34,12 @@ function Signup() {
             <label>Confirm Password</label>
             <input
               type="Email"
-              className="bg-gray-200 p-3 rounded w-96"
+              className="bg-gray-200 p-3 rounded w-[33rem]"
               placeholder="Re-Enter your Password"
             />
           </div>
           <div className='my-2'>
-            <button className="bg-black text-white w-96 p-2 rounded-2xl">Register</button>
+            <button className="bg-black text-white w-[33rem] p-2 rounded-2xl">Register</button>
           </div>
         </form>
       </div>
