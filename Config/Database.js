5287fd1597abb7d2a72ff4f11ded0a875cf9ecf7
ali-raza-baig <alirazabaigmdk@gmail.com>
2024-11-26 22:54:13 +0500

    e

--- Config/Database.js
+++ Config/Database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 console.log(process.env.MONGOURI)
 const DbConnection = async () => {
     try {
-        mongoose.connect(process.env.MONGOURI)
+        mongoose.connect("mongodb+srv://alirazabaigmdk:A1L2i3r4@test-cluster.fo5kjwp.mongodb.net/Auth-App?retryWrites=true&w=majority&appName=Test-Cluster")
 
         console.log('Database Connected')
     } catch (error) {
