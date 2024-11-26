66e0c5a58a819cf509ff1e2e203ff1fa65fefbcc
ali-raza-baig <alirazabaigmdk@gmail.com>
2024-11-26 22:50:50 +0500

    test

--- Config/Database.js
+++ Config/Database.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
+console.log(process.env.MONGOURI)
 const DbConnection = async () => {
     try {
         mongoose.connect(process.env.MONGOURI)
+
         console.log('Database Connected')
     } catch (error) {
         console.log(error)
